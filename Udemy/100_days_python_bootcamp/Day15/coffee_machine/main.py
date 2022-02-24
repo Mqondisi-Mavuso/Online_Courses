48a5579999988e778918923a2bf42c0004348b15
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-02-24 23:46:08 +0800

    Day 15, Coffee machine

---
+++ Udemy/100_days_python_bootcamp/Day15/coffee_machine/main.py
@@ -0,0 +1,10 @@
+#from art import logo
+from data import MENU, resources
+
+# TODO 1: Take user's coffee order and compare it against the resourses and the money they have
+machine_resources = resources
+
+order = input("What would you like to have? 1. Espresso, 2. Latte, 3. Cappuccino? ")
+# TODO 2: Check if the money the user has will be enough for the order or not
+# TODO 3: Check if the resources are enough for the order or not
+# TODO 4: If both todo 2 and 3 are met, their coffee will be made using the machine resources
