2cda378caa91d35c6c195e6fdf33fc7cfc30bbd4
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-03-08 23:52:12 +0800

    Second pong game commit

--- Udemy/100_days_python_bootcamp/Day22/ball.py
+++ Udemy/100_days_python_bootcamp/Day22/ball.py
@@ -7,14 +7,21 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move = 1
-        self.y_move = 1
+        self.x_move = 2
+        self.y_move = 2
 
-    def move(self):
+    def move_right(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def move_left(self):
+        new_x = self.xcor() - self.x_move
+        new_y = self.ycor() - self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
         self.y_move *= -1
 
+    def bounce_x(self):
+        self.x_move *= -1
--- Udemy/100_days_python_bootcamp/Day22/main.py
+++ Udemy/100_days_python_bootcamp/Day22/main.py
@@ -19,15 +19,28 @@ screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 
-screen.onkey(l_paddle.go_up, "W")
-screen.onkey(l_paddle.go_down, "S")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.0001)
+    time.sleep(0.00001)
     screen.update()
-    ball.move()
+    ball.move_right()
+
+    # for bouncing off the top wall
     if ball.ycor() < -280 or ball.ycor() > 280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # for bouncing off the paddles
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320 or ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    if ball.xcor() < l_paddle.xcor() or ball.xcor() > r_paddle.xcor():
+        ball.goto(0, 0)
+        if ball.xcor() < l_paddle.xcor():
+            ball.move_right()
+        elif ball.xcor() > r_paddle.xcor():
+            ball.move_right()
 
 screen.exitonclick()
