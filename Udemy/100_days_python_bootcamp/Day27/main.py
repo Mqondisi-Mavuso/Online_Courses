cac57e2ea2f0be78f5895810cf57830eb70a2079
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-03-28 01:02:07 +0800

    Miles to Km final project
    My first Tkinter GUI project

--- Udemy/100_days_python_bootcamp/Day27/main.py
+++ Udemy/100_days_python_bootcamp/Day27/main.py
@@ -1,11 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My first GUI program")
+window = Tk()
+window.title("Mile to Km Converter")          # For changing the title of the window
 window.minsize(width=500, height=300)
 
 # label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()                 # This allows the label to be shown on the screen
+click_variable = ""
+answer = Label(text="", font=("Arial", 24, "bold"))
+answer.grid(column=1, row=1)                # This allows the label to be shown on the screen
+
+miles = Label(text="Miles", font=("Arial", 24, "bold"))
+miles.grid(column=2, row=0)                # This allows the label to be shown on the screen
+
+kilometers = Label(text="Km", font=("Arial", 24, "bold"))
+kilometers.grid(column=2, row=1)                # This allows the label to be shown on the screen
+
+equals = Label(text="is equal to", font=("Arial", 24, "bold"))
+equals.grid(column=0, row=1)                # This allows the label to be shown on the screen
+
+def click():
+    outcome = round(float(entry.get())*1.60934, 2)      # gets the string from the entry box and calculate the Km
+    answer.config(text =str(outcome))                   # change the answer label text to the answer
+
+my_button = Button(text="Calculate", command=click)     # command listens to the mouse click from use and call click()
+my_button.grid(column=1, row=2)
+
+entry = Entry()
+entry.grid(column=1, row=0)
 
 window.mainloop()
