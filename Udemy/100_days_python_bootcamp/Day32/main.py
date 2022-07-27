299d19cf0a26ca3656ddad2cb4c25ce804fc8d5e
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-07-28 01:23:53 +0800

    API endpoints final project
    This code runs infinite and sends an email if ever the International Space Station (ISS) is above where I am in the sky at night

--- Udemy/100_days_python_bootcamp/Day32/main.py
+++ Udemy/100_days_python_bootcamp/Day32/main.py
@@ -1,40 +1,80 @@
-# Automated email birthday wishes project
-# day 32 of 100
-import random
-import datetime as dt
+# day 33
+# API endpoints, ISS above notifier 
+
+import requests
+from datetime import datetime as dt
 import smtplib
-import pandas as pd
+import time
 
-# 1. Update the birthdays.csv
+LATITUDE = 23.901471
+LONGITUDE = 121.546151
+LOCAL_UTC_OFFSET = 8
 
-# 2. Check if today matches a birthday in the birthdays.csv
-today_tuple = (dt.datetime.now().month, dt.datetime.now().day)                  # making a date tuple
 
-try:
-    data = pd.read_csv("birthdays.csv")
-    birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-except FileNotFoundError as error:
-    print(f"{error}")
-else:
-    if today_tuple in birthdays_dict:                   # checks if today's date matches anyone's birthday
-        recipient = birthdays_dict[today_tuple]       # stores the record of the person with the date we're looking for
-        print(recipient)
-        file_dir = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-        try:
-            with open(file_dir) as letter_file:
-                message = letter_file.read()
-                message = message.replace("[NAME]", recipient["name"])
-        except FileNotFoundError as the_error:
-            print(the_error)
-        else:
-            my_email = "mavusomqondisi@gmail.com"
-            password = "bgrrqsjcusdknsmb"
+def utc_to_local(utc_hour):
+    # this is for converting from UCT to my local timezone
+    utc_hour += LOCAL_UTC_OFFSET
+    if LOCAL_UTC_OFFSET > 0:
+        if utc_hour > 23:
+            utc_hour -= 24
+    elif LOCAL_UTC_OFFSET < 0:
+        if utc_hour < 0:
+            utc_hour += 24
+    return utc_hour
 
-            with smtplib.SMTP("smtp.gmail.com") as connection:
-                connection.starttls()                                                   # for encryption purposes
-                connection.login(user=my_email, password=password)                      # to logging my gmail account
-                connection.sendmail(
-                    from_addr=my_email,
-                    to_addrs=recipient["email"],
-                    msg=f"Subject:Birthday Wishes\n\n{message}."
-                )
+
+def iss_above():
+    if LATITUDE - 5 <= iss_latitude <= LATITUDE + 5:
+        if LONGITUDE - 5 <= iss_longitude <= LONGITUDE + 5:
+            return True
+    else:
+        return False
+
+
+def is_night():
+    if time_now >= lt_sunset or time_now < lt_sunrise:
+        return True
+    else:
+        return False
+
+
+# ---------------------------------Getting local time and position ---------------------------
+parameters = {
+    "lat": LATITUDE,
+    "lng": LONGITUDE,
+    "formatted": 0,
+}
+
+response = requests.get(url="http://api.sunrise-sunset.org/json?", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+lt_sunrise = utc_to_local(sunrise)
+lt_sunset = utc_to_local(sunset)
+time_now = dt.now().time().hour
+
+# -------------------------------------------------Getting ISS location --------------------
+iss_data = requests.get(url="http://api.open-notify.org/iss-now.json").json()
+
+iss_latitude = float(iss_data["iss_position"]["latitude"])
+iss_longitude = float(iss_data["iss_position"]["longitude"])
+
+# --------------------------SEND E-MAIL----------------------------------------------------#
+# this while loop allows the code to run forever in the background every 60 seconds
+while True:
+    time.sleep(60)
+    if iss_above() and is_night():
+        my_email = "mavusomqondisi@gmail.com"
+        password = "bgrrqsjcusdknsmb"
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()  # for encryption purposes
+            connection.login(user=my_email, password=password)  # to logging my gmail account
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="fortunemavuso4@gmail.com",
+                msg=f"Subject:International Space Station (ISS) \n\n Look up the ISS is currently above you!."
+            )
