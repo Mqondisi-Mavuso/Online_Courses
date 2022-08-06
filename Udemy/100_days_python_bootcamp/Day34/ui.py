06ef92c42cefc199848fc12afd04ac9fa02ec356
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-08-07 04:08:51 +0800

    Only need to work on the buttons

--- Udemy/100_days_python_bootcamp/Day34/ui.py
+++ Udemy/100_days_python_bootcamp/Day34/ui.py
@@ -1,27 +1,39 @@
 from tkinter import *
 from data import question_data
 import requests
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#0C7B93"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain                 # Used to tap into the quiz_brain class
         self.window = Tk()
         self.window.title("Trivier")
-        self.window.config(padx=50, pady=50, bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
         # this is for the front part of the flash card
         self.canvas = Canvas(width=300, height=250)
         self.canvas.config(highlightthickness=0, bg="#D3E0EA")
-        self.canvas.grid(column=0, row=1, columnspan=2)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
         # self.tria_question = question_data.["question"]
-        # self.trivial_question = self.canvas.create_text(400, 150, text=self.tria_question, font=("Ariel", 5, "italic"))
+        self.trivial_question = self.canvas.create_text(
+            150,
+            125,
+            width=250,
+            text="Questions will be here" ,
+            font=("Ariel", 20, "italic"), fill=THEME_COLOR
+        )
 
         # score text
-        self.GUI_text = Label(self.window, text="Score: 0", font=("Courier", 14), bg=THEME_COLOR)
-        self.GUI_text.grid(row=0, column=1)
+        self.score_text = Label(self.window,
+                                text="Score: 0",
+                                font=("Courier", 14),
+                                bg=THEME_COLOR, fg="white"
+                                )
+        self.score_text.grid(row=0, column=1)
 
         # True button
         self.correct_image = PhotoImage(file="images/true.png")
@@ -33,4 +45,13 @@ class QuizInterface:
         self.correct_button = Button(image=self.wrong_image, highlightthickness=0, bg=THEME_COLOR)
         self.correct_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.trivial_question, text=q_text)
+
+
