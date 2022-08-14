fc39b45dfa01f66833d07be8408e6e424fc68ced
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-08-14 23:59:42 +0800

    update

--- Udemy/100_days_python_bootcamp/Day36/main.py
+++ Udemy/100_days_python_bootcamp/Day36/main.py
@@ -1,4 +1,5 @@
 import requests
+from newsapi import NewsApiClient
 import itertools
 import time
 
@@ -9,11 +10,18 @@ api_key_alpha_vantage= "7Z4L3XBAPF5T4ABG"
 api_key_news = "627e693128694d3c9c765bfd939c788d"
 
 alpha_parameters = {
-    "function": "FX_INTRADAY",
-    "from_symbol": "USD",
-    "to_symbol": "TWD",
+    "function": "TIME_SERIES_DAILY",
+    "symbol": "AAPL",
     "interval": "60min",
     "apikey": api_key_alpha_vantage
+}
+
+news_parameters = {
+    "q": "apple",
+    "from": "2022-08-11",
+    "to": "2022-08-12",
+    "sortBy": "popularity",
+    "apiKey": api_key_news
 }
 
 ## STEP 1: Use https://www.alphavantage.co
@@ -21,28 +29,31 @@ alpha_parameters = {
 response = requests.get(url="https://www.alphavantage.co/query", params=alpha_parameters)
 alpha_data = response.json()
 
-
+print(alpha_data)
 # first_two_days = dict(itertools.islice(alpha_data["Time Series FX (60min)"].items(),2))
-yesterday_midday_value = float(alpha_data["Time Series FX (60min)"]["2022-08-11 12:00:00"]["4. close"])
-previousday_midday_value = float(alpha_data["Time Series FX (60min)"]["2022-08-10 12:00:00"]["4. close"])
+yesterday_close_value = float(alpha_data["Time Series (Daily)"]["2022-08-12"]["4. close"])
+previousday_close_value = float(alpha_data["Time Series (Daily)"]["2022-08-11"]["4. close"])
 
-percentage_diff = round(((yesterday_midday_value - previousday_midday_value)/previousday_midday_value) * 100, 1)
+percentage_diff = round(((yesterday_close_value - previousday_close_value)/previousday_close_value) * 100, 1)
 
-print(f"Yesterday's midday value market: {yesterday_midday_value}")
-print(f"Previous day mid\day market value: {previousday_midday_value}")
-
-print(percentage_diff)
+print(f"Yesterday's midday value market: {yesterday_close_value}")
+print(f"Previous day mid\day market value: {previousday_close_value}")
+#
+# print(percentage_diff)
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
+
 if -2.0 > percentage_diff or percentage_diff > 2.0:
-    print("Get news")
+    news_response = requests.get(url="https://newsapi.org/v2/everything", params=news_parameters)
+    news_data = news_response.json()
+
+    print(news_data)
 else:
     pass
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
 
 #Optional: Format the SMS message like this: 
 """
