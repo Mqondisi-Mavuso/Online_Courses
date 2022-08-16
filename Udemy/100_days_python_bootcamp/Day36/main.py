e7ee396b6e49b5ee4093501cd6c2bcd54b991784
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-08-16 22:00:32 +0800

    Everything is fine but also not fine

--- Udemy/100_days_python_bootcamp/Day36/main.py
+++ Udemy/100_days_python_bootcamp/Day36/main.py
@@ -1,48 +1,83 @@
-# Day 35 of 100
-# Rain notification app
-
-import os
 import requests
 from twilio.rest import Client
 
-LATITUDE = "23.901498"
-LONGITUDE = "121.545543"
+STOCK = "AAPL"
+COMPANY_NAME = "Apple Inc"
 
-twilio_api_key = "1dfdbce83d8a64bd5cff74a68cd11421"
-twilio_account_sid = "ACbcf14bb937f2aaed3e59cf43cca9340d"
-twilio_auth_token = "65cbcad85db4b987c28b2e4d693ccf09"
-twilio_number = "+19302122954"
+api_key_alpha_vantage = "7Z4L3XBAPF5T4ABG"
+api_key_news = "627e693128694d3c9c765bfd939c788d"
+
+alpha_parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "interval": "60min",
+    "apikey": api_key_alpha_vantage
+}
+
+news_parameters = {
+    "qInTitle": COMPANY_NAME,
+    "apiKey": api_key_news
+}
+
+twilio_api_key = "SK8c0f01671c4b3b22c11f547a9f64cf85"
+twilio_account_sid = "AC10b33f5534d919776cf0e16ed8bc81f9"
+twilio_auth_token = "8bdfe9cd4bbc4e6cff88844570403b17"
+twilio_number = "+18593282421"
 twilio_recovery_code = "lTGyyU2nftGITYcFiSV9xFv8ESYV8-ClDGKhoIK_"
 
 twilio_parameters = {
-    "lat": LATITUDE,
-    "lon": LONGITUDE,
-    "appid": api_key,
-    "exclude": "current,minutely,daily"
+    "appid": twilio_api_key,
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
-response.raise_for_status()                             # for raising an exception just in case we get wrong http code
-weather_data = response.json()
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+response = requests.get(url="https://www.alphavantage.co/query", params=alpha_parameters)
+alpha_data = response.json()["Time Series (Daily)"]
 
-twelve_h_list = weather_data["hourly"][0:12]
-weather_id_list = []
-for i in range(0, 12):
-    weather_id_list.append(twelve_h_list[i]["weather"][0]["id"])        # getting the weather id for the next 12 hours
+list_data = [value for (key, value) in alpha_data.items()]          # using list comprehension to convert dict to list
+yesterday_dict = list_data[0]
+yest_close_value = float(yesterday_dict["4. close"])
+previous_day_dict = list_data[1]
+previousday_close_value = float(previous_day_dict["4. close"])
 
-will_rain = False
-for id in weather_id_list:
-    if id < 700:
-        will_rain = True
+percentage_diff = round(((yest_close_value - previousday_close_value)/previousday_close_value) * 100, 1)
+#
+print(f"Yesterday's midday value market: {yest_close_value}")
+print(f"Previous day mid\day market value: {previousday_close_value}")
+print(percentage_diff)
 
-if will_rain:
-    client = Client(account_sid, auth_token)
+# ## STEP 2: Use https://newsapi.org
+# # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+#
+#
+if 0 > percentage_diff or percentage_diff > 0:
+    news_response = requests.get(url="https://newsapi.org/v2/everything", params=news_parameters)
+    news_data = news_response.json()["articles"][:3]
 
-    message = client.messages \
-        .create(
-        body="Mqondisi, It is going to rain today remember to bring an umbrella ☂️!",
-        from_=twilio_number,
-        to='+8860988030913'
-    )
+    if percentage_diff < 0:
+        final_articles = [
+            f"AAPL 🔻{percentage_diff}%\nHeadline: {article['title']}. \nBrief: {article['description']}"
+            for article in news_data]
+    else:
+        final_articles = [
+            f"AAPL 🔺{percentage_diff}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for
+            article in news_data]
 
-    print(message.sid)
+    client = Client(twilio_account_sid, twilio_auth_token)
+    for message_body in final_articles:
+        print(message_body)
+        message = client.messages.create(
+            body=message_body,
+            from_=twilio_number,
+            to='+8860988030913'
+        )
+
+        # print(message.error_message)
+else:
+    pass
+
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+#Optional: Format the SMS message like this:
+
