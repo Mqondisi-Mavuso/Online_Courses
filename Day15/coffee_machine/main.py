05894b8b1f60dcbcd20d50af58b9f8c5a006bcaa
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2023-12-17 23:53:05 +0800

    Update main.py

--- Day15/coffee_machine/main.py
+++ Day15/coffee_machine/main.py
@@ -4,7 +4,7 @@ from data import MENU, resources
 # TODO 1: Take user's coffee order and compare it against the resourses and the money they have
 machine_resources = resources
 
-order = input("What would you like to have? 1. Espresso, 2. Latte, 3. Cappuccino? ")
+order = input("What would you like to have? 1. Espresso, 2. Latte, 5. Cappuccino? ")
 # TODO 2: Check if the money the user has will be enough for the order or not
 # TODO 3: Check if the resources are enough for the order or not
 # TODO 4: If both todo 2 and 3 are met, their coffee will be made using the machine resources
